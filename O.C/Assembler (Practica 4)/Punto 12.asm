16f34fd896bb485966896be8cac0c77887a9648d
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-06-19 00:05:30 -0300

    Create Punto 12.asm

---
+++ O.C/Assembler (Practica 4)/Punto 12.asm
@@ -0,0 +1,3 @@
+; Escribir un programa que, dado un número X, genere un arreglo con todos los resultados que se obtienen hasta llegar
+;a 0, aplicando la siguiente fórmula: si X es par, se le resta 7; si es impar, se le suma 5, y al resultado se le aplica
+;nuevamente la misma fórmula. Ej: si X = 3 entonces el arreglo tendrá: 8, 1, 6, -1, 4, -3, 2, -5, 0.
